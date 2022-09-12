7ef87f6d17ca6ac5d5354b9dad4c36495bc09ece
tomohiro1024 <gongyezhihong732@gmail.com>
2022-09-12 18:30:53 +0900

    Bottom()の作成

--- lib/pages/screen.dart
+++ lib/pages/screen.dart
@@ -8,12 +8,22 @@ class ScreenPage extends StatefulWidget {
 }
 
 class _ScreenPageState extends State<ScreenPage> {
+  List<IconData> bottomNavIcons = [
+    Icons.house,
+    Icons.theaters,
+    Icons.add_box,
+    Icons.shopping_cart,
+    Icons.person,
+  ];
+
+  int selectedIndex = 0;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.white,
       appBar: AppBar(
-        actionsIconTheme: IconThemeData(color: Colors.lightBlue),
+        actionsIconTheme: IconThemeData(color: Colors.black),
         backgroundColor: Colors.white,
         elevation: 0,
         title: const Text(
@@ -44,6 +54,24 @@ class _ScreenPageState extends State<ScreenPage> {
             ),
           ),
         ],
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        type: BottomNavigationBarType.fixed,
+        unselectedIconTheme: const IconThemeData(color: Colors.black),
+        selectedIconTheme: const IconThemeData(color: Colors.black),
+        items: List.generate(
+          bottomNavIcons.length,
+          (index) => BottomNavigationBarItem(
+            icon: Icon(bottomNavIcons[index]),
+            label: '',
+          ),
+        ),
+        currentIndex: selectedIndex,
+        onTap: (newIndex) {
+          setState(() {
+            selectedIndex = newIndex;
+          });
+        },
       ),
     );
   }
